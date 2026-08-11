f7be2f28720be5fa5d50d16bf75b69af04e30173
Mirko Kontroller <Mirko_Kontroller@proton.me>
2026-08-12 01:46:14 +0200

    Change background image and blur in glass effect
    Updated background image and blur effect for glass effect.

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ document.head.appendChild(pseudo);
 
 pseudo.innerHTML = `
     .glass::before{
-        background-image: url('https://cdn.pixabay.com/photo/2015/12/15/05/43/starry-night-1093721_960_720.jpg');
+        background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Claude_Monet_-_The_Houses_of_Parliament%2C_London_%28Metropolitan_Museum_of_Art%29.jpg/1920px-Claude_Monet_-_The_Houses_of_Parliament%2C_London_%28Metropolitan_Museum_of_Art%29.jpg');
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
-        -webkit-filter: blur(5px);
-        filter: blur(5px);
+        -webkit-filter: blur(6px);
+        filter: blur(6px);
     }
 `;
