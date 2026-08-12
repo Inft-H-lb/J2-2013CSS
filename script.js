3bd8f3ecd09a4fdc8ddaaceb3d70c4d7bbe0da6a
Mirko Kontroller <Mirko_Kontroller@proton.me>
2026-08-12 03:34:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const pseudo = document.createElement('style');
-document.head.appendChild(pseudo);
 
-pseudo.innerHTML = `
-    .glass::before{
-        background-image: url('./bilder/Westminster.jpg');
-        background-size: cover;
-        background-position: center;
-        background-attachment: fixed;
-        -webkit-filter: blur(6px);
-        filter: blur(6px);
-    }
-`;
